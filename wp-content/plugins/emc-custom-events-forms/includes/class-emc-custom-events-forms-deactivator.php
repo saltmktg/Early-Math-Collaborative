cbbdfb01b3deed621a071e8d1b4842934a23d71a
Laura Melo <lauramelos@gmail.com>
2015-12-30 12:14:28 -0300

    plugin init

---
+++ wp-content/plugins/emc-custom-events-forms/includes/class-emc-custom-events-forms-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    EMC_CustomEventsForms
+ * @subpackage EMC_CustomEventsForms/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    EMC_CustomEventsForms
+ * @subpackage EMC_CustomEventsForms/includes
+ * @author     Your Name <email@example.com>
+ */
+class EMC_CustomEventsForms_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}

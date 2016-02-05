2a48215dbe4df4a582c44b20084a91a1e291853a
Paulo Ricardo Lopes Iankoski <pauloiankoski@gmail.com>
2016-02-04 23:06:08 -0200

    Change organizer label and remove some fields from new event form.

---
+++ wp-content/themes/emc/tribe-events/community/modules/organizer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Event Submission Form Metabox For Organizers
+ * This is used to add a metabox to the event submission form to allow for choosing or
+ * creating an organizer for user submitted events.
+ *
+ * Override this template in your own theme by creating a file at
+ * [your-theme]/tribe-events/community/modules/organizer.php
+ *
+ * @package Tribe__Events__Community__Main
+ * @since  2.1
+ * @author Modern Tribe Inc.
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+if ( ! isset( $event ) ) {
+	$event = Tribe__Events__Main::postIdHelper();
+}
+?>
+
+<!-- Organizer -->
+<div class="tribe-events-community-details eventForm bubble" id="event_organizer">
+
+	<table class="tribe-community-event-info" cellspacing="0" cellpadding="0">
+
+		<thead> <tr>
+			<td colspan="2" class="tribe_sectionheader">
+				<h4> <label class="<?php echo tribe_community_events_field_has_error( 'organizer' ) ? 'error' : ''; ?>">Assigned Coach</label> </h4>
+			</td><!-- .tribe_sectionheader -->
+		</tr> </thead>
+
+		<?php
+		// The organizer meta box will render everything within a <tbody>
+		$organizer_meta_box = new EMC_Tribe__Events__Admin__Organizer_Chooser_Meta_Box( $event );
+		$organizer_meta_box->render();
+		?>
+
+	</table> <!-- #event_organizer -->
+
+</div>
